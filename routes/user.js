7eeb6ce88472b73a2aaa6c8adf5e9c68d162e508
Your Name~ahmadjamal <you@example.com>
2024-01-22 17:03:42 +0500

    user route configured

--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,16 @@ const { Router } = require("express");
 const router = Router();
 
 // Middlewares
-
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const roleMiddleware = require("../middlewares/authorizeRoles");
+const upload = multer({ storage });
+const checkUser = require("../middlewares/checkUser");
 // Controllers
 const controller = require("../controllers/user");
-// const upload = require("../middlewares/uploadFile");
-const putSignedUrl = require("../middlewares/putObject");
-const multer = require("multer");
-const storage=multer.memoryStorage()
-const upload= multer({storage})
+//courses Router
+const courseRouter = require("./course");
+
 // Routes
 router.post(
   "/signup/:__something",
@@ -18,4 +20,15 @@ router.post(
 );
 router.post("/login", controller.login);
 router.get("/", controller.getUsers);
+// router.post("/forget-Password",constroller.forgetPassword);
+// router.post("/rest-pasword/:token",constroller.forgetPassword)
+router.get("/logout", controller.logout);
+router.patch(
+  "/:id/updateStatus/:userId",
+  checkUser,
+  roleMiddleware("ADMIN"),
+  controller.updateStatus
+);
+router.use("/:id/course", checkUser, courseRouter);
+
 module.exports = router;
