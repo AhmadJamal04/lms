08c31d2384b07d3573778e6ef174b4f14cf4164b
Your Name~ahmadjamal <you@example.com>
2024-01-24 12:20:42 +0500

    routes for future use

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router = Router();
 
 // Routers
 const userRouter = require("./user");
+const courseRouter= require("./course")
+const enrolementRouter=require("./enrolement")
+
 
 router.use("/users", userRouter);
+router.use("/courses", courseRouter);
+router.use("/enrolement", enrolementRouter);
 
 module.exports = router;
