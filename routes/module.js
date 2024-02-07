c92eb8f6ea9c5be4ddf2debf69181bb33a4c3e33
AhmadJamal04 <engr.jamal04@gmail.com>
2024-02-06 16:57:29 -0800

    module routes configured

--- routes/module.js
+++ routes/module.js
@@ -1,7 +1,11 @@
 const{Router}=require("express");
 const roleMiddleware = require("../middlewares/authorizeRoles");
+const checkUser = require("../middlewares/checkUser");
 const router= Router();
-router.post("/", roleMiddleware("INSTRUCTOR"),)
+
+const controller= require("../controllers/module")
+router.post("/:id/:course_id",checkUser, roleMiddleware("INSTRUCTOR"),controller.newModule)
+router.delete("/:id/:module_id",checkUser,roleMiddleware("INSTRUCTOR"),controller.deleteMOdule)
 
 
 module.exports=router;
