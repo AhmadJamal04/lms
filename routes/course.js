846a61e0df443e0c5bd376295562272416c348a0
AhmadJamal04 <engr.jamal04@gmail.com>
2024-02-06 16:58:27 -0800

    minor changes done in course route

--- routes/course.js
+++ routes/course.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 const controller = require("../controllers/course");
 const roleMiddleware = require("../middlewares/authorizeRoles");
-const moduleRouter= require("./module")
+const checkUser = require("../middlewares/checkUser");
+
 const router = Router();
 
-router.post("/", roleMiddleware("INSTRUCTOR"), controller.newCourse);
+router.post("/:id",checkUser, roleMiddleware("INSTRUCTOR"), controller.newCourse);
+router.delete("/:id/:course_id",checkUser, roleMiddleware("ADMIN"), controller.deleteCourse);
 
-router.delete("/:courseId", roleMiddleware("ADMIN"), controller.deleteCourse);
-router.use("/:courseId/module",moduleRouter )
 module.exports = router;
