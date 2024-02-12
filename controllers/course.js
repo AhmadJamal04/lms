a511b716222d93582a0fbbf33ce8e8f3ff64ad39
AhmadJamal04 <engr.jamal04@gmail.com>
2024-02-12 15:52:35 -0800

    update-course, fineOne-course apis

--- controllers/course.js
+++ controllers/course.js
@@ -1,41 +1,90 @@
 const { generateErrorInstance } = require("../utils");
-const { Courses } = require("../models");
+const { Courses,Modules } = require("../models");
+const { where } = require("sequelize");
 
 module.exports = {
   newCourse: async (req, res, next) => {
     try {
       const id = req.user.id;
       const { title, course_intro, description } = req.body;
-      const course=await Courses.create({
+      const course = await Courses.create({
         title,
         course_intro,
         description,
         fk_instructor_id: id,
       });
       res.status(200).json({
-        success:true,
-        data:course
-      })
+        success: true,
+        data: course,
+      });
     } catch (error) {
       next(error);
     }
   },
-  deleteCourse:async(req,res,next)=>{
+  deleteCourse: async (req, res, next) => {
     try {
-        const{course_id}=req.params;
-    const course= await Courses.findByPk(course_id)
-    if(!course){
+      const { course_id } = req.params;
+      const course = await Courses.findByPk(course_id);
+      if (!course) {
         throw generateErrorInstance({
-            status:404,
-            message:"no course found"
-        })
-    }
-   
-  await course.destroy();
-    res.status(200).send("course deleted successfully")
+          status: 404,
+          message: "no course found",
+        });
+      }
+      await course.destroy();
+      res.status(200).send("course deleted successfully");
     } catch (error) {
-        console.log(error);
-        next(error);
+      console.log(error);
+      next(error);
+    }
+  },
+  updateCourse: async (req, res, next) => {
+    try {
+      const { title, description } = req.body;
+      const { id, course_id } = req.params;
+      const course = await Courses.findByPk(course_id);
+      if (!course) {
+        throw generateErrorInstance({
+          status: 404,
+          message: "no course found",
+        });
+      }
+      const updatedCourse = await Courses.update(
+        {
+          title,
+          description,
+        },
+        { where: { fk_instructor_id: id }, individualHooks: true }
+      );
+      res.status(200).json({
+        status: "success",
+        data: updatedCourse,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+  myCourse:async(req,res,next)=>{
+    try {
+      const{id,course_id}=req.params;
+      let course=await Courses.findOne({
+        where:{fk_instructor_id:id,
+        id:course_id},
+        include : {
+          model: Modules,
+          as: "modules",
+        }
+      });
+      if (!course) {
+        throw generateErrorInstance({
+          status:404,
+          message:"course not found"
+        })
+        
+      }
+      res.send(course)
+    } catch (error) {
+      next(error)
     }
   }
 };
