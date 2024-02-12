9f3a5278aa54515e287b1cf9d2707c1b0e3a5b6f
AhmadJamal04 <engr.jamal04@gmail.com>
2024-02-12 15:53:11 -0800

    update and get courses routes added

--- routes/course.js
+++ routes/course.js
@@ -6,6 +6,8 @@ const checkUser = require("../middlewares/checkUser");
 const router = Router();
 
 router.post("/:id",checkUser, roleMiddleware("INSTRUCTOR"), controller.newCourse);
+router.patch("/:id/:course_id",checkUser,roleMiddleware("ADMIN","INSTRUCTOR"),controller.updateCourse)
+router.get("/:id/:course_id",checkUser,roleMiddleware("INSTRUCTOR"),controller.myCourse)
 router.delete("/:id/:course_id",checkUser, roleMiddleware("ADMIN"), controller.deleteCourse);
 
 module.exports = router;
