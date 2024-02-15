27a38dadce376d11b79075bc55474562f5a1f362
AhmadJamal04 <engr.jamal04@gmail.com>
2024-02-15 12:01:57 -0800

    this will be done in future

--- routes/user.js
+++ routes/user.js
@@ -6,26 +6,27 @@ const multer = require("multer");
 const storage = multer.memoryStorage();
 const roleMiddleware = require("../middlewares/authorizeRoles");
 const upload = multer({ storage });
+const { roles } = require("../config");
 const checkUser = require("../middlewares/checkUser");
 // Controllers
 const controller = require("../controllers/user");
 
 // Routes
 router.post(
-  "/signup/:__something",
+  "/signup/:__something", //move this to req body
   upload.single("profileImage"),
   controller.signup
 );
 router.post("/login", controller.login);
 router.post("/getPresignedUrl", controller.preSignedUrl);
 router.get("/", controller.getUsers);
-router.post("/forget-Password", controller.forgetPassword);
+router.post("/forgetPassword", controller.forgetPassword);
 // router.post("/rest-pasword/:token",controller.resetPassword)
 router.get("/logout", controller.logout);
 router.patch(
   "/:id/updateStatus/:userId",
   checkUser,
-  roleMiddleware("ADMIN"),
+  roleMiddleware(roles.admin),
   controller.updateStatus
 );
 
